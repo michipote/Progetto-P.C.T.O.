e75f2b20f71b54650306453ba72ca8031610979b
karanbir-singh <karans1723@gmail.com>
2021-04-17 12:48:24 +0200

    Modify schema

--- server-side/sql/tamponi.sql
+++ server-side/sql/tamponi.sql
@@ -16,6 +16,66 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `account`
+--
+
+DROP TABLE IF EXISTS `account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `account` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tipo` int(1) NOT NULL,
+  `nome` varchar(45) NOT NULL,
+  `cognome` varchar(45) NOT NULL,
+  `fiscale` varchar(16) NOT NULL,
+  `password` varchar(61) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `account`
+--
+
+LOCK TABLES `account` WRITE;
+/*!40000 ALTER TABLE `account` DISABLE KEYS */;
+/*!40000 ALTER TABLE `account` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `assegnato`
+--
+
+DROP TABLE IF EXISTS `assegnato`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `assegnato` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `data` date NOT NULL,
+  `fk_admin` int(11) NOT NULL,
+  `fk_staff` int(11) NOT NULL,
+  `fk_sede` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `assegnato_account_id_fk` (`fk_admin`),
+  KEY `assegnato_account_id_fk_2` (`fk_staff`),
+  KEY `assegnato_sede_id_fk` (`fk_sede`),
+  CONSTRAINT `assegnato_account_id_fk` FOREIGN KEY (`fk_admin`) REFERENCES `account` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `assegnato_account_id_fk_2` FOREIGN KEY (`fk_staff`) REFERENCES `account` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `assegnato_sede_id_fk` FOREIGN KEY (`fk_sede`) REFERENCES `sede` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `assegnato`
+--
+
+LOCK TABLES `assegnato` WRITE;
+/*!40000 ALTER TABLE `assegnato` DISABLE KEYS */;
+/*!40000 ALTER TABLE `assegnato` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `prenotazione`
 --
 
@@ -23,13 +83,65 @@ DROP TABLE IF EXISTS `prenotazione`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `prenotazione` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `codice_fiscale` varchar(16) NOT NULL,
-  `giorno` date NOT NULL,
-  `codice_prenotazione` varchar(100) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `data` date NOT NULL,
+  `fiscale` varchar(16) NOT NULL,
+  `univoco` varchar(20) NOT NULL,
+  `data_prenotazione` date NOT NULL,
+  `stato` int(1) NOT NULL,
+  `fk_sede` int(11) NOT NULL,
+  `fk_personale` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `prenotazione_codice_prenotazione_uindex` (`codice_prenotazione`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+  UNIQUE KEY `univoco_UNIQUE` (`univoco`),
+  KEY `fk_sede_idx` (`fk_sede`),
+  KEY `fk_personale_idx` (`fk_personale`),
+  CONSTRAINT `fk_personale` FOREIGN KEY (`fk_personale`) REFERENCES `account` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_sede` FOREIGN KEY (`fk_sede`) REFERENCES `sede` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
--- Dump completed on 2021-03-20 10:01:31
+--
+-- Dumping data for table `prenotazione`
+--
+
+LOCK TABLES `prenotazione` WRITE;
+/*!40000 ALTER TABLE `prenotazione` DISABLE KEYS */;
+/*!40000 ALTER TABLE `prenotazione` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sede`
+--
+
+DROP TABLE IF EXISTS `sede`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sede` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Indirizzo` varchar(100) NOT NULL,
+  `nome` varchar(45) NOT NULL,
+  `longitudine` double NOT NULL,
+  `latitudine` double NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sede`
+--
+
+LOCK TABLES `sede` WRITE;
+/*!40000 ALTER TABLE `sede` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sede` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-04-17 12:46:58
