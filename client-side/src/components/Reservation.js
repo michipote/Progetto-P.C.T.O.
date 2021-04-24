3b68ea66e31c938c3b8e89c632300d6f86325723
karanbir-singh <karans1723@gmail.com>
2021-04-24 10:43:15 +0200

    Work on DatePicker and unavailable dates

--- client-side/src/components/Reservation.js
+++ client-side/src/components/Reservation.js
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from "react";
-import ReactDOM from "react-dom";
 import { Form, Input, Button, InputNumber, DatePicker } from 'antd';
 import Layout, { Content, Footer, Header } from "antd/lib/layout/layout";
+import moment from "moment";
 const { getData, postData } = require('../ws');
 
 // Form di prenotazione
 export default function Reservation(props) {
+    // Date non disponibili
+    const [unavailableDates, setUnavailableDates] = useState(null);
+
+    // Limiti (min e max) del DatePicker, DAL GIORNO CORRENTE (OGGI)
+    const min = 3;
+    const max = 17;
+
+    // Proprietà layout
     const layout = {
         labelCol: {
             span: 0,
@@ -15,6 +23,7 @@ export default function Reservation(props) {
         },
     };
 
+    // Config del DatePicker
     const config = {
         rules: [
             {
@@ -25,22 +34,43 @@ export default function Reservation(props) {
         ],
     };
 
+    // Messaggio visualizzato se trova dei campi vuoti, ma obbligatori
     const validateMessages = {
         required: 'Campo obbligatorio',
     };
+    
+    // Disabilita nel DatePicker le date non disponibili
+    const disabledDate = (current) => {
+        // Date da disabilitare, ottenute dal server database
+        let datesToDisable = unavailableDates?.map(elem => elem.data);
 
+        // Data da disabilitare
+        let date = datesToDisable?.find((elem) => moment(current).format('YYYY-MM-DD') === elem);
+
+        // Ritorna true per:
+        // - tutte le date successive la giorno corrente di 3 (per adesso) giorni
+        // - se current (ogni data del mese) equivale ad una data disabilitata
+        return current < moment().add(min, 'days')
+            || current > moment().add(max, 'days')
+            || (current > moment(date) && current < moment(date).add(1, 'days'));
+    };
+
+    // Funzione chiamata una volta fatto il submit
     const onFinish = (values) => {
         let formattedValues = { ...values, data: values.data.format('YYYY-MM-DD') };
 
         postData('http://localhost:63342/server-side/lista_prenotazioni.php', formattedValues)
             .then(data => {
-                console.log(data);
+                // console.log(data); RISPOSTA
             });
     };
 
-    // useEffect(() => {
-    //     getUnavailableDates('http://localhost:63342/server-side/nonDisponibili.php');
-    // }, [])
+    // Una volta montato il componente, fa un rischiesta al server database per ottenere le date non disponibili
+    useEffect(() => {
+        getData('http://localhost:63342/server-side/nonDisponibili.php').then(data => {
+            setUnavailableDates(data);
+        });
+    }, [])
 
     return (
         <>
@@ -63,7 +93,7 @@ export default function Reservation(props) {
                         </Form.Item>
 
                         <Form.Item name={['data']} label="Data" {...config} >
-                            <DatePicker placeholder="Scegli la data" format="DD MMMM YYYY" />
+                            <DatePicker placeholder="Scegli la data" format="DD MMMM YYYY" disabledDate={disabledDate} />
                         </Form.Item>
 
                         <Form.Item
---
+++ server-side/nonDisponibili.php
@@ -0,0 +1,11 @@
+<?php
+include_once "./config.php";
+$stmt = $pdo->query("
+SELECT data,  count(*) as occupati
+FROM prenotazione
+WHERE stato != 2
+GROUP BY data
+HAVING occupati >= 3 
+    OR ( DATEDIFF(data, CURRENT_DATE()) < 3 AND DATEDIFF(data, CURRENT_DATE()) > 11) 
+");
+echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
