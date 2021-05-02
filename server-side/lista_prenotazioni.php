75e3d439314d8fecef6fc9c086ee19d528e91cd6
DarthTarr <patrik.maniu@gmail.com>
2021-05-03 01:13:54 +0200

    Elenco di tutte le prenotazioni indifferentemente da stato e sede

--- server-side/lista_prenotazioni.php
+++ server-side/lista_prenotazioni.php
@@ -1,28 +1,6 @@
 <?php
-require './vendor/autoload.php';
-include_once "config.php";
-
-use League\Plates\Engine;
-
-// Viene creato l'oggetto per la gestione dei template
-// view e' la cartella che contiene il template file
-// tpl e' l'estensione del template file
-$templates = new Engine('./view', 'tpl');
-
-// Query di inserimento preparata
-$sql = "SELECT * FROM prenotazione";
-
-// Invio la query al server MySQL
-$stmt = $pdo->query($sql);
-
-// Estraggo le righe di risposta che finiranno come vettori in $result
-$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-if(isset($_SESSION['username'])){ // Se sei una utente che ha fatto il login
-    $username = $_SESSION['username'];
-
-    //Rendo un template che mi visualizza la tabella
-    echo $templates->render('lista_prenotazioni', ['result' => $result, 'username' => $username]);
-}
+include_once "./config.php";
+$stmt = $pdo->query("SELECT * FROM prenotazione");
+echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
 
 
