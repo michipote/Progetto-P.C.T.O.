093e5e0af9d257fa1db7ff304694768bfbb561a3
DarthTarr <patrik.maniu@gmail.com>
2021-05-07 06:14:18 +0200

    Limita le date in cui si può prenotare data la sede

--- server-side/listaPrenotazioniLimitata.php
+++ server-side/listaPrenotazioniLimitata.php
@@ -2,7 +2,8 @@
 include_once "./config.php";
 $_POST = json_decode(file_get_contents("php://input"), true);
 $data = [
-    'fiscale' => $_POST['codice']
+    'fiscale' => $_POST['codice'],
+    'sede' => $_POST['sede']
 ];
 $sql = "SELECT prenotazione.* 
 FROM account, assegnato, sede, prenotazione
@@ -10,6 +11,7 @@ WHERE account.fiscale = :fiscale
 AND account.id = assegnato.fk_staff 
 AND sede.id = assegnato.fk_sede
 AND sede.id = prenotazione.fk_sede
+AND sede.id = :sede
 AND prenotazione.stato = 0
 AND prenotazione.data_prenotazione = CURRENT_DATE()";
 $stmt = $pdo->prepare($sql)->execute($data);
