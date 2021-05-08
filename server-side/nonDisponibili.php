91fb5ac7551bf881abc8a07db7607d6bbce1fc8f
karanbir-singh <karans1723@gmail.com>
2021-05-08 10:18:08 +0200

    Update this

--- server-side/nonDisponibili.php
+++ server-side/nonDisponibili.php
@@ -1,10 +1,19 @@
 <?php
 include_once "./config.php";
-$stmt = $pdo->query("SELECT data,  count(*) as occupati
+//$_POST = json_decode(file_get_contents("php://input"), true);
+
+//TODO Fare Join con la sede e tornare le date NON DISPONIBILI in base alla sede
+$sql = "
+SELECT data,  count(*) as occupati
 FROM prenotazione
 WHERE stato != 2
 GROUP BY data
-HAVING occupati >= 3
-   AND DATEDIFF(data, CURRENT_DATE()) > 3
-   AND DATEDIFF(data, CURRENT_DATE()) < 11");
+HAVING occupati >= 3 
+    OR ( DATEDIFF(data, CURRENT_DATE()) < 3 AND DATEDIFF(data, CURRENT_DATE()) > 11)
+";
+
+//$stmt = $pdo->prepare($sql);
+//$result = $stmt->execute([]);
+$stmt = $pdo->query($sql);
+
 echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
