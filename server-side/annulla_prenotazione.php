10206c023be236ed290c178cad5bc15f491b8104
DarthTarr <patrik.maniu@gmail.com>
2021-05-11 22:09:23 +0200

    Requires both unique code and personal id to cancel a reservation

--- server-side/annulla_prenotazione.php
+++ server-side/annulla_prenotazione.php
@@ -3,6 +3,7 @@ include_once "./config.php";
 $_POST = json_decode(file_get_contents("php://input"), true);
 $data = [
     'codice_prenotazione' => $_POST['codice'],
+    'fiscale' => $_POST['fiscale']
 ];
-$sql = "UPDATE prenotazione SET stato = 2 WHERE univoco = :codice_prenotazione AND stato = 0";
-$pdo->prepare($sql)->execute($data);
+$sql = "UPDATE prenotazione SET stato = 2 WHERE univoco = :codice_prenotazione AND stato = 0 AND fiscale = :fiscale";
+echo json_encode($pdo->prepare($sql)->execute($data));
