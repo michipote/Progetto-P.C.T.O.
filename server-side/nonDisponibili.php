4acac781d51a99327cd8fd7f44443cf745798206
DarthTarr <patrik.maniu@gmail.com>
2021-05-11 19:58:46 +0200

    Returns the dates that are not available for reservations given an office

--- server-side/nonDisponibili.php
+++ server-side/nonDisponibili.php
@@ -1,19 +1,16 @@
 <?php
 include_once "./config.php";
-//$_POST = json_decode(file_get_contents("php://input"), true);
-
-//TODO Fare Join con la sede e tornare le date NON DISPONIBILI in base alla sede
+$_POST = json_decode(file_get_contents("php://input"), true);
+$sede = $_POST['sede'];
 $sql = "
 SELECT data,  count(*) as occupati
-FROM prenotazione
+FROM prenotazione, sede
 WHERE stato != 2
+AND sede.id = '$sede'
 GROUP BY data
 HAVING occupati >= 3 
     OR ( DATEDIFF(data, CURRENT_DATE()) < 3 AND DATEDIFF(data, CURRENT_DATE()) > 11)
 ";
-
-//$stmt = $pdo->prepare($sql);
-//$result = $stmt->execute([]);
 $stmt = $pdo->query($sql);
 
 echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
