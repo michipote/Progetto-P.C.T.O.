85684cab0795502c63c1e71247c69336a933e724
DarthTarr <patrik.maniu@gmail.com>
2021-05-13 06:28:57 +0200

    Checks if teh office in a given date is full before creating a reservation

--- server-side/prenota.php
+++ server-side/prenota.php
@@ -8,6 +8,26 @@ $data = [
     'data_prenotazione' => $_POST['data'],
     'sede' => $_POST['sede']
 ];
-$sql = "INSERT INTO prenotazione (data, fiscale, univoco, data_prenotazione, stato, fk_sede) VALUES (current_date(), :fiscale, :univoco, :data_prenotazione, 0, :sede)";
-$pdo->prepare($sql)->execute($data);
-echo json_encode($ritorno);
+$sql = "SELECT COUNT(*) AS persone FROM prenotazione, sede WHERE data_prenotazione = :data_prenmotazione AND sede.id = prenotazione.fk_sede";
+$stmt = $pdo->prepare($sql);
+
+$stmt->execute(
+    [
+        'data_prenotazione' => $data['data_prenotazione']
+    ]
+);
+$riga = $stmt->fetch();
+
+$numero_persone = $riga['persone'];
+
+if ($numero_persone >= 3)
+{
+    echo '{"result" : "failed"
+            "motivo" : "sedepiena"}';
+}
+else
+{
+    $sql = "INSERT INTO prenotazione (data, fiscale, univoco, data_prenotazione, stato, fk_sede) VALUES (current_date(), :fiscale, :univoco, :data_prenotazione, 0, :sede)";
+    $pdo->prepare($sql)->execute($data);
+    echo json_encode($ritorno);
+}
