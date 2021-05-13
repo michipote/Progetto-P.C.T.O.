1a4dedae86888c5b246ec76f4b8d8f456892583f
karanbir-singh <karans1723@gmail.com>
2021-05-13 19:25:22 +0200

    Update query

--- server-side/nonDisponibili.php
+++ server-side/nonDisponibili.php
@@ -3,13 +3,11 @@ include_once "./config.php";
 $_POST = json_decode(file_get_contents("php://input"), true);
 $sede = $_POST['sede'];
 $sql = "
-SELECT data,  count(*) as occupati
-FROM prenotazione, sede
-WHERE stato != 2
-AND sede.id = '$sede'
-GROUP BY data
-HAVING occupati >= 3 
-    OR ( DATEDIFF(data, CURRENT_DATE()) < 3 AND DATEDIFF(data, CURRENT_DATE()) > 11)
+SELECT data_prenotazione AS data_disabilitata
+FROM prenotazione
+WHERE prenotazione.fk_sede = '$sede'
+GROUP BY data_disabilitata
+HAVING COUNT(*) >= 10
 ";
 $stmt = $pdo->query($sql);
 
