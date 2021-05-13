debd82dddea1d730aa88735ff7c0653f3f2cf44a
karanbir-singh <karans1723@gmail.com>
2021-05-13 22:27:14 +0200

    Fix script

--- server-side/prenota.php
+++ server-side/prenota.php
@@ -1,33 +1,34 @@
 <?php
 include_once "./config.php";
-$ritorno->codice = substr(uniqid(uniqid(), true), -30);
-$_POST = json_decode(file_get_contents("php://input"), true);
+//$fiscale = $_POST['fiscale'];
+//$data_prenotazione = $_POST['data_prenotazione'];
+//$id_sede = $_POST['sede'];
+$fiscale = 'VGDLPP74S14A564T';
+$data_prenotazione = '2021-05-20';
+$id_sede = 1;
+
+$sql = "
+SELECT data_prenotazione AS data_disabilitata
+FROM prenotazione
+WHERE prenotazione.fk_sede = '$id_sede'
+GROUP BY data_disabilitata
+HAVING COUNT(*) >= 10";
+$stmt = $pdo->query($sql);
+
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    if ($data_prenotazione === $row['data_disabilitata']) {
+        echo '{ "risultato" : "failed", "motivo" : "Data con limite di prenotazioni raggiunto" }';
+        exit(0);
+    }
+}
+
 $data = [
-    'fiscale' => $_POST['fiscale'],
-    'univoco' => $ritorno->codice,
-    'data_prenotazione' => $_POST['data'],
-    'sede' => $_POST['sede']
+    'fiscale' => $fiscale,
+    'univoco' => substr(uniqid(uniqid(), true), -30),
+    'data_prenotazione' => $data_prenotazione,
+    'id_sede' => $id_sede
 ];
-$sql = "SELECT COUNT(*) AS persone FROM prenotazione, sede WHERE data_prenotazione = :data_prenotazione AND sede.id = prenotazione.fk_sede";
-$stmt = $pdo->prepare($sql);
 
-$stmt->execute(
-    [
-        'data_prenotazione' => $data['data_prenotazione']
-    ]
-);
-$riga = $stmt->fetch();
-
-$numero_persone = $riga['persone'];
-
-if ($numero_persone >= 3)
-{
-    echo '{"result" : "failed"
-            "motivo" : "sedepiena"}';
-}
-else
-{
-    $sql = "INSERT INTO prenotazione (data, fiscale, univoco, data_prenotazione, stato, fk_sede) VALUES (current_date(), :fiscale, :univoco, :data_prenotazione, 0, :sede)";
-    $pdo->prepare($sql)->execute($data);
-    echo json_encode($ritorno);
-}
+$sql = "INSERT INTO prenotazione (data, fiscale, univoco, data_prenotazione, stato, fk_sede) VALUES (CURRENT_DATE(), :fiscale, :univoco, :data_prenotazione, 0, :id_sede)";
+$pdo->prepare($sql)->execute($data);
+echo '{"risultato" : "succ"}';
