7066cdb63b7bb2c309ea25c9ef048112f2e4a4bf
karanbir-singh <karans1723@gmail.com>
2021-05-13 22:51:29 +0200

    Fix script

--- server-side/prenota.php
+++ server-side/prenota.php
@@ -1,11 +1,8 @@
 <?php
 include_once "./config.php";
-//$fiscale = $_POST['fiscale'];
-//$data_prenotazione = $_POST['data_prenotazione'];
-//$id_sede = $_POST['sede'];
-$fiscale = 'VGDLPP74S14A564T';
-$data_prenotazione = '2021-05-20';
-$id_sede = 1;
+$fiscale = $_POST['fiscale'];
+$data_prenotazione = $_POST['data_prenotazione'];
+$id_sede = $_POST['sede'];
 
 $sql = "
 SELECT data_prenotazione AS data_disabilitata
@@ -22,13 +19,14 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     }
 }
 
+$univoco = substr(uniqid(uniqid(), true), -30);
 $data = [
     'fiscale' => $fiscale,
-    'univoco' => substr(uniqid(uniqid(), true), -30),
+    'univoco' => $univoco,
     'data_prenotazione' => $data_prenotazione,
     'id_sede' => $id_sede
 ];
 
 $sql = "INSERT INTO prenotazione (data, fiscale, univoco, data_prenotazione, stato, fk_sede) VALUES (CURRENT_DATE(), :fiscale, :univoco, :data_prenotazione, 0, :id_sede)";
 $pdo->prepare($sql)->execute($data);
-echo '{"risultato" : "succ"}';
+echo "{'risultato' : 'succ','univoco': '$univoco'}";
